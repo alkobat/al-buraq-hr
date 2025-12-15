8a025a800764e9af1a083d9dfd7c2c36f45cc2cd
alkobat <alkobat@gmail.com>
2025-12-16 00:59:17 +0200

    Delete update_evaluation_method.sql

--- update_evaluation_method.sql
+++
@@ -1,4 +0,0 @@
--- إضافة إعداد طريقة احتساب التقييم إلى جدول system_settings
-INSERT INTO system_settings (`key`, value) 
-VALUES ('evaluation_method', 'manager_only')
-ON DUPLICATE KEY UPDATE value = value;
